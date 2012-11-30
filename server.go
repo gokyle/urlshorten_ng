fa028e386cbdbb9e6ab934e93d31db3413af9209
Kyle Isom <coder@kyleisom.net>
2012-11-29 20:51:10 -0700

    using new webcache app branch

--- server.go
+++ server.go
@@ -26,6 +26,7 @@ func init() {
 }
 
 func main() {
+	var host, port string
 	conf, err := config.ParseFile(config_file)
 	if err != nil {
 		fmt.Printf("[!] couldn't parse config file: %s\n", err.Error())
@@ -33,19 +34,19 @@ func main() {
 	}
 
 	if conf["server"] == nil {
-		webshell.SERVER_ADDR = DEFAULT_HOST
-		webshell.SERVER_PORT = DEFAULT_PORT
+		host = DEFAULT_HOST
+		port = DEFAULT_PORT
 	} else {
 		if conf["server"]["port"] != "" {
-			webshell.SERVER_PORT = conf["server"]["port"]
+			port = conf["server"]["port"]
 		} else {
-			webshell.SERVER_PORT = DEFAULT_PORT
+			port = DEFAULT_PORT
 		}
 
 		if conf["server"]["host"] != "" {
-			webshell.SERVER_ADDR = conf["server"]["host"]
+			host = conf["server"]["host"]
 		} else {
-			webshell.SERVER_ADDR = DEFAULT_HOST
+			port = DEFAULT_HOST
 		}
 
 		if conf["server"]["development"] == "false" {
@@ -99,7 +100,7 @@ func main() {
 	}
 
 	if server_dev {
-		server_host = fmt.Sprintf("%s:%s", server_host, webshell.SERVER_PORT)
+		server_host = fmt.Sprintf("%s:%s", server_host, port)
 	}
 
 	NotFound, err = webshell.GenerateTemplateErrorHandler(http.StatusNotFound,
@@ -107,14 +108,13 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	webshell.StaticRoute("/assets/", "assets/")
-	webshell.AddRoute("/", topRoute)
-	if check_auth {
-		webshell.AddRoute("/change", changePass)
-		webshell.AddRoute("/add", addUser)
-	}
-	webshell.AddRoute("/views/", getViews)
-	webshell.Serve(false, nil)
+	app := webshell.NewApp("urlshorten-ng", host, port)
+	app.StaticRoute("/assets/", "assets/")
+	app.AddRoute("/", topRoute)
+	app.AddConditionalRoute(check_auth, "/add", addUser)
+	app.AddConditionalRoute(check_auth, "/change", changePass)
+	app.AddRoute("/views/", getViews)
+	app.Serve()
 }
 
 func config_server() {
